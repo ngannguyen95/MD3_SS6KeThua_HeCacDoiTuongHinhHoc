7ea9a52b7185c64bd7c53043cebec6a0a40ec4fd
Nguyencongnguyen1812 <ngannt33@wru.vn>
2023-03-16 15:39:39 +0700

    hhi

--- src/rikkei/academy/Main.java
+++ src/rikkei/academy/Main.java
@@ -1,46 +1,66 @@
 package rikkei.academy;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         // kiểm thử class
-        System.out.println("-------SHAPE--------");
-        Shape shape = new Shape();
-        System.out.println(shape);
+//        System.out.println("-------SHAPE--------");
+//        Shape shape = new Shape();
+//        System.out.println(shape);
+//
+//        shape = new Shape("red", false);
+//        System.out.println(shape);
+//
+//        // kiểm thử Class Circle
+//        System.out.println("-------CIRCLE-------");
+//        Circle circle = new Circle();
+//        System.out.println(circle);
+//
+//        circle = new Circle(3.5);
+//        System.out.println(circle);
+//
+//        circle = new Circle(3.5, "indigo", false);
+//        System.out.println(circle);
+//
+//        //RECTANGLE
+//        System.out.println("-----RECTANGLE----");
+//        Rectangle rectangle = new Rectangle();
+//        System.out.println(rectangle);
+//
+//        rectangle = new Rectangle(2.3, 5.8);
+//        System.out.println(rectangle);
+//
+//        rectangle = new Rectangle(2.5, 3.8, "orange", true);
+//        System.out.println(rectangle);
+//
+//        // SQUARE
+//        System.out.println("----SQUARE-------");
+//        Square square = new Square();
+//        System.out.println(square);
+//
+//        square = new Square(2.3);
+//        System.out.println(square);
+//
+//        square=new Square(5.8,"yellow",true);
+//        System.out.println(square);
 
-        shape = new Shape("red", false);
-        System.out.println(shape);
+        System.out.println("----COMPARABLE------");
+        ComparableCircle[] circles=new ComparableCircle[3];
+        circles[0]=new ComparableCircle(3.6);
+        circles[1]=new ComparableCircle();
+        circles[2]=new ComparableCircle(3.5,"red",false);
 
-        // kiểm thử Class Circle
-        System.out.println("-------CIRCLE-------");
-        Circle circle = new Circle();
-        System.out.println(circle);
+        System.out.println(" pre-sorted :");
+        for (ComparableCircle comparableCircle:circles) {
+            System.out.println(comparableCircle);
+        }
+        Arrays.sort(circles);
 
-        circle = new Circle(3.5);
-        System.out.println(circle);
-
-        circle = new Circle(3.5, "indigo", false);
-        System.out.println(circle);
-
-        //RECTANGLE
-        System.out.println("-----RECTANGLE----");
-        Rectangle rectangle = new Rectangle();
-        System.out.println(rectangle);
-
-        rectangle = new Rectangle(2.3, 5.8);
-        System.out.println(rectangle);
-
-        rectangle = new Rectangle(2.5, 3.8, "orange", true);
-        System.out.println(rectangle);
-
-        // SQUARE
-        System.out.println("----SQUARE-------");
-        Square square = new Square();
-        System.out.println(square);
-
-        square = new Square(2.3);
-        System.out.println(square);
-
-        square=new Square(5.8,"yellow",true);
-        System.out.println(square);
+        System.out.println("after- sorted: ");
+        for (ComparableCircle comparableCircle:circles
+             ) {
+            System.out.println(comparableCircle);
+        }
     }
 }
